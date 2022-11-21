8dab541f065c8ebb73424041386288a7acfbb064
nelly476 <nellybulatova12@gmail.com>
2022-11-21 20:18:05 +0300

    add random dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import Die from "./components/Die";
+import "./App.css";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      let num = Math.floor(Math.random() * 6) + 1;
+      newDice.push({
+        value: num,
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return newDice;
+  }
+
+  const [dice, setDice] = useState(allNewDice());
+
+  const diceElements = dice.map((number) => {
+    return <Die value={number.value} key={number.id} />;
+  });
+
+  function changeDice() {
+    setDice(allNewDice());
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={changeDice}>
+        Roll
+      </button>
+    </main>
+  );
 }
 
-export default App
+export default App;
